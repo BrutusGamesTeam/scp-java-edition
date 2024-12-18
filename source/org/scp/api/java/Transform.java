85c7c3d2563452de52b981baf9a52ea94a3a94ce
BrutusGamesTeam <oficiallazarentos@gmail.com>
2024-12-17 22:53:03 -0300

    Create Transform.java

---
+++ source/org/scp/api/java/Transform.java
@@ -0,0 +1,11 @@
+// Pacote do Código
+package org.scp.api.java;
+
+// Importações Explícitas
+import org.joml.Vector3f;
+
+// Classe da Transformação Tri-Dimensional e Bi-Dimensional
+public class Transform
+{
+    
+}
