4cd24013898bf017eda740a0cced39525a3c0e7d
BrutusGamesTeam <oficiallazarentos@gmail.com>
2025-02-12 07:18:32 -0300

    Update Transform.java

--- source/org/scp/api/java/Transform.java
+++ source/org/scp/api/java/Transform.java
@@ -7,5 +7,8 @@ import org.joml.Vector3f;
 // Classe da Transformação Tri-Dimensional e Bi-Dimensional
 public class Transform
 {
-    
+    // Campos Importantes
+
+    // Posição em Vetor Tri-Dimensional
+    public Vector3f position_vector = new Vector3f(0f, 0f, 0f);
 }
