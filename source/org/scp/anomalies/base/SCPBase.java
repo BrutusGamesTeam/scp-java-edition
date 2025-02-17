aca63c9c1e4127c2080902d17ced0bf4bc6b3e15
BrutusGamesTeam <oficiallazarentos@gmail.com>
2025-02-17 17:54:46 -0300

    Update SCPBase.java

--- source/org/scp/anomalies/base/SCPBase.java
+++ source/org/scp/anomalies/base/SCPBase.java
@@ -1,1 +1,15 @@
+// Pacote do Código
+package org.scp.anomalies.base;
 
+// Classe Responsável por Representar uma Anomalia S.C.P
+public class SCPBase
+{
+    // Campos Exclusívos da Classe
+
+    // Identificação da Anomalia S.C.P
+    private String scp_anomalyId = "Null S.C.P Anomaly";
+
+    // Classe de Objeto da Anomalia S.C.P
+    // Indo de SAFE para EXPLAINED
+    private SCPObjectClass scp_objectClass = SCPObjectClass.NULL;
+}
