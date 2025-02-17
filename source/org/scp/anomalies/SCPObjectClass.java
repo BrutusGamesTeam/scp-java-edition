a3db97071d8b3a51a8a5e41f71b9d24590532e1a
BrutusGamesTeam <oficiallazarentos@gmail.com>
2025-02-17 18:08:51 -0300

    Update SCPObjectClass.java

--- source/org/scp/anomalies/SCPObjectClass.java
+++ source/org/scp/anomalies/SCPObjectClass.java
@@ -4,27 +4,27 @@ package org.scp.anomalies;
 // Enumerador (Grupo de Definições Diferentes) de Classes de Anomalias S.C.P
 public enum SCPObjectClass
 {
-    // Seguro: Anomalias que não Exigem Cuidados Excessivos para Manipulação
+    // Seguro: Não Exigem Cuidados Excessivos para Manipulação
     SAFE,
 
-    // Euclídeo: Anomalias que Exigem um Cuidado Intermediário ao Manipular
+    // Euclídeo: Exigem um Cuidado Intermediário ao Manipular
     EUCLID,
 
-    // Keter (do Indonédio: "Ter"): Anomalias que são Extremamente Difíceis de se Manipular
+    // Keter (do Indonédio: "Ter"): Extremamente Difíceis de se Manipular
     KETER,
 
-    // Apocalíptico: Anomalias que NÃO devem ser Contidas em Hipótese Alguma devido seu Risco de Destruição Planetária
+    // Apocalíptico: Não devem ser Contidas devido seu Risco de Destruição Planetária
     APOLLYON,
 
-    // Thaumiel: Anomalias que podem ser Úteis na Contenção de Anomalias, Principalmente em Contra-Ataques
+    // Thaumiel: Úteis na Contenção de Anomalias, Principalmente em Contra-Ataques
     THAUMIEL,
 
-    // Neutralizado: Anomalias Extintas que foram Perdidas ou Radicalizadas com Sucesso
+    // Neutralizado: Extintas que foram Perdidas ou Radicalizadas com Sucesso
     NEUTRALIZED,
 
-    // Desconhecido: Anomalias que não há Enquadramendo Claro de qual Classe deve-se Colocar
+    // Desconhecido: Não há Enquadramendo Claro de qual Classe deve-se Colocar
     UNKNOWN,
 
-    // Nulo: Anomalias que não foram Especificadas no Código
+    // Nulo: Não foram Especificadas no Código
     NULL
 }
