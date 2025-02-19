46eb9cbf0679ac06c58933752236b88a6b79f168
BrutusGamesTeam <oficiallazarentos@gmail.com>
2025-02-19 19:16:24 -0300

    Create Vertex3D.java

---
+++ source/org/scp/api/java/Vertex3D.java
@@ -0,0 +1,18 @@
+// Pacote do Código
+package org.scp.api.java;
+
+// Importações Explícitas
+import java.util.ArrayList;
+import java.util.List;
+
+// Classe Responsável pela Representação de Modelos Tri-Dimensionais
+public class Vertex3D
+{
+    // Campos Importantes
+
+    // Lista de Vértices do Modelo
+    private List<int> model_vertices = new ArrayList<int>();
+
+    // Lista de Triangulos do Modelo
+    private List<int> model_triangles = new ArrayList<int>();
+}
