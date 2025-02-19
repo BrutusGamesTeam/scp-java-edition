5da2eacc1841277cb265f1e9154f498da0403a26
BrutusGamesTeam <oficiallazarentos@gmail.com>
2025-02-19 20:02:58 -0300

    Create Entity.java

---
+++ source/org/scp/api/java/basic/Entity.java
@@ -0,0 +1,11 @@
+// Pacote do Código
+package org.scp.api.java.basic;
+
+// Importações Explícitas
+import org.scp.api.java.basic.IEntity;
+
+// Classe Responsável pela Criação de Entidades
+public class Entity
+{
+    
+}
