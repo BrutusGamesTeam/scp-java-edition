744e769bb7a1c71fa8bd413b6c59d2875c8e790c
BrutusGamesTeam <oficiallazarentos@gmail.com>
2025-03-01 19:39:43 -0300

    Create ModelRenderer3D.java

---
+++ source/org/scp/api/java/rendererer/ModelRenderer3D.java
@@ -0,0 +1,17 @@
+// Pacote do Código
+package org.scp.api.java.renderer;
+
+// Importações Explícitas
+import org.scp.api.java.Mesh3D;
+
+// Classe de Utilidade Tri-Dimensional
+public final class ModelRenderer3D
+{
+    // Construtores Básicos
+
+    // Cria uma nova Instância do Renderizador
+    public ModelRenderer3D()
+    {
+        
+    }
+}
