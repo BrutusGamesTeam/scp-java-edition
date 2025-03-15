b147e0f6a42d3b0be170bd037a9ffbcb21674921
BrutusGamesTeam <oficiallazarentos@gmail.com>
2025-03-15 16:42:23 -0300

    Update ModelRenderer3D.java

--- source/org/scp/api/java/rendererer/ModelRenderer3D.java
+++ source/org/scp/api/java/rendererer/ModelRenderer3D.java
@@ -5,13 +5,47 @@ package org.scp.api.java.renderer;
 import org.scp.api.java.Mesh3D;
 
 // Classe de Utilidade Tri-Dimensional
-public final class ModelRenderer3D
+public class ModelRenderer3D
 {
+    // Campos Privados
+
+    // Modelo Anexado ao Renderizador
+    private Mesh3D mesh3D = null;
+    
     // Construtores Básicos
 
-    // Cria uma nova Instância do Renderizador
+    /**
+     * Cria uma nova Instância do Renderizador
+     */
     public ModelRenderer3D()
     {
         
+    }
+
+    // Funções Declaradas
+
+    /**
+     * Obtém o Modelo do Renderizador Tri-Dimensional
+     * @return O Modelo que está Anexado
+     */
+    public Mesh3D mr_getMesh3D()
+    {
+        return this.mesh3D;
+    }
+
+    /**
+     * Mude o Modelo do Renderizador Tri-Dimensional
+     * @param value O novo Modelo para Anexar
+     */
+    public void mr_setMesh3D(Mesh3D value)
+    {
+        // Valide o Valor para Anexar
+        if(value == null)
+        {
+            // Lance uma Exceção de Ponteiro Nulo
+            throw new NullPointerException("[Exceção] > Não foi possível mudar o valor do modelo anexado ao renderizador atual.");
+        }
+        
+        this.mesh3D = value;
     }
 }
